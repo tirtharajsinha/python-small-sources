e476a38bf17d3df0719255456c2ef83a43807b78
Tirtharaj Sinha <64787936+tirtharajsinha@users.noreply.github.com>
2020-09-30 22:44:29 +0530

    Update mailing-with-smtplib.py

--- mailing-with-smtplib.py
+++ mailing-with-smtplib.py
@@ -1,19 +1,29 @@
 import smtplib
 print("mail sender system")
-sender=input("enter your mail id : ")
+# to use this snippet more efficient way save a mail gmail id as string in 'serderd' variable 
+sender=input("enter your mail id (if default type only '1'): ").strip()
+if sender=="1":
+    senderd="tsinha098@gmail.com"
+    sender=senderd
 reciver=input("enter reciver's mail id : ")
-
-message=input("enter the message here(without newline):")
-try:
-    passward=input("enter passward :")
+sub="Subject:"+input("enter subject of the mail:")+"\n"
+message=sub+input("enter the message here(without newline):")+"\n This message is auto-generated.sent by Tirtharaj_sinha-official.If you think this is mistake,ignore"
+try: 
+    passward=input("enter passward of your id:")
     smtpObj=smtplib.SMTP('smtp.gmail.com' , 587)
     smtpObj.starttls()
     smtpObj.login(sender,passward)
-    smtpObj.sendmail(sender,reciver,message)
-    print("sucessfully mail sent")
-    smtpObj.quit()
+    try:
+        smtpObj.sendmail(sender,reciver,message)
+        print("sucessfully mail sent")
+        smtpObj.quit()
+    except Exception:
+        print("Error !!  unable to send mail")
+        smtpObj.quit()
+    
     
 except Exception:
-    print("Error !! unable to sent mail.")
+    print("Error !! unable to login")
+    
     smtpObj.quit()
     
